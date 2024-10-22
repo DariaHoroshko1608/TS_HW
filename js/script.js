5a74bd9e614ce4ea2e625e470112dd56ddfb17b5
Dasha Horoshko <schekachovadasha1608@gmail.com>
2024-10-22 15:09:27 +0300

    hw_4

--- js/script.js
+++ js/script.js
@@ -1,30 +1,51 @@
-const BankAccount = (function() {
-    let balance = 0;
+"use strict";
 
-    return {
-        deposit: function(amount) {
-            if (amount > 0) {
-                balance += amount;
-                console.log(`Депозит: ${amount}. Новий баланс: ${balance}`);
-            } else {
-                console.log("Сума депозиту повинна бути позитивною.");
-            }
-        },
-        withdraw: function(amount) {
-            if (amount > 0 && amount <= balance) {
-                balance -= amount;
-                console.log(`Виведення: ${amount}. Новий баланс: ${balance}`);
-            } else {
-                console.log("Недостатньо коштів або неправильна сума для виведення.");
-            }
-        },
-        getBalance: function() {
-            return balance;
+class EventManager {
+    constructor() {
+        this.events = {};
+    }
+    on(eName, cb) {
+        if (!this.events[eName]) {
+            this.events[eName] = [];
         }
-    };
-})();
+        this.events[eName].push(cb);
+    }
+    off(eName, cb) {
+        if (!this.events[eName]) return;
 
-const myAccount = BankAccount;
-myAccount.deposit(100);
-myAccount.withdraw(30);
-console.log(myAccount.getBalance());
+        if (cb) {
+            this.events[eName] = this.events[eName].filter(callback => callback !== cb);
+        }
+
+        if (!cb) {
+            delete this.events[eName];
+        }
+    }
+    trigger(eName, ...args) {
+        if (!this.events[eName]) return;
+
+        this.events[eName].forEach(callback => {
+            callback.apply(this, args);
+        });
+    }
+}
+
+const eventManager = new EventManager();
+
+const greetings = function(name) {
+    console.log(`hello ${name}!`);
+};
+
+const goodBye = function(name) {
+    console.log(`GoodBye ${name}!`);
+};
+
+eventManager.on('greetings', greetings);
+eventManager.trigger('greetings', 'Dasha');
+
+eventManager.on('goodBye', goodBye);
+eventManager.trigger('goodBye', 'Dasha');
+
+eventManager.off('goodBye', goodBye);
+eventManager.trigger('goodBye', 'Dasha');
+
